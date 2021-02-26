c6ec90f3313a5a16cf308e5f63839cd5b0b4c730
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-02-26 11:37:51 -0300

    feat: dev

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :rocketpay, Rocketpay.Repo,
   username: "postgres",
-  password: "senha",
+  password: "docker",
   database: "rocketpay_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
