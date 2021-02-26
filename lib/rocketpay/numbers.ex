6144b436dfd1676d4a4919643df1a2bb07b311e3
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-02-26 11:38:38 -0300

    fix numbers

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -8,10 +8,12 @@ defmodule Rocketpay.Numbers do
   defp handle_file({:ok, result}) do
     result =
       result
-      |> String.split(",")
-      |> Stream.map(fn number -> String.to_integer(number) end)
-      |> Enum.sum()
+    |> String.split(",")
+    |> Stream.map(fn number -> String.to_integer(number) end)
+    |> Enum.sum()
+
     {:ok, %{result: result}}
   end
-  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
+
+  defp handle_file({:error, _reason}), do: {:error, %{ message: "Invalid file!"}}
 end
